0c23dea5378523f0288eeff633f78ad059158187
Jia <yijh1705@gmail.com>
2023-12-26 20:54:52 -0500

    Finish JS in script.js, makes game playable

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const playAgain = document.querySelector(".playAgain");
+
+const endContainer = document.querySelector(".endContainer");
+const container = document.querySelector(".container");
+
+const player = document.querySelector("#player");
+const computer = document.querySelector("#computer");
+
+const statusDiv = document.querySelector(".status");
+
+let winner = '';
+let playerScore = 0;
+let computerScore = 0;
+
+
+
 function getComputerChoice() {
     // chooses a random number from 0 to 2, inclusive
     let choice = Math.floor(Math.random() * 3);
@@ -17,50 +36,76 @@ function playRound(playerSelection, computerSelection) {
     let newPlayerSelection = playerSelection.toLowerCase();
 
     if (newPlayerSelection == computerSelection) {
-        return "tied"
+        winner = "tie";
     } else if ((newPlayerSelection == "rock" && computerSelection == "scissors") ||
                 (newPlayerSelection == "scissors" && computerSelection == "paper") ||
                 (newPlayerSelection == "paper" && computerSelection == "rock")) {
-        return "player"
+        playerScore++;
+        winner = "player";
     } else {
-        return "computer"
+        computerScore++;
+        winner = "computer";
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
 
-    while (playerScore < 5 && computerScore < 5) {
-        let playerSelection = prompt("Please enter your selection: rock, paper, or scissors?");
-        console.log(`You chose chose: ${playerSelection}.`)
+function handleClick(playerChoice) {
+    let computerChoice = getComputerChoice();
 
+    playRound(playerChoice, computerChoice);
 
-        let computerSelection = getComputerChoice();
-
-        console.log(`Computer chose: ${computerSelection}.`)
-
-        let winner = playRound(playerSelection, computerSelection);
-
-        if (winner == "tied") {
-            console.log(`You tied! Play again ;3`)
-            break;
-        } else if (winner == "player") {
-            playerScore++;
-            console.log(`You won!`)
-        } else {
-            computerScore++;
-            console.log("You lost!")
-        }
-
-        console.log(`Current Score: You: ${playerScore}, Computer: ${computerScore}`);
-    }
-
-    if (playerScore == 3) {
-        console.log(`The winner is: You!`);
+    if (playerScore == 5) {
+        statusDiv.innerText =  "You beat the computer!"
+        showEndScreen();
+    } else if (computerScore == 5) {
+        statusDiv.innerText =  "The computer beat you!"
+        showEndScreen();
+    } else if (winner == "tie") {
+        console.log(`The computer chose ${computerChoice}. You tied! Play again ;3`)
+        statusDiv.innerText = `The computer chose ${computerChoice}. You tied! Play again ;3`;
+    } else if (winner == "player") {
+        statusDiv.innerText = `The computer chose ${computerChoice}. You won!`;
+        console.log(`The computer chose ${computerChoice}. You won!`)
     } else {
-        console.log(`The winner is: the Computer!`);
+        statusDiv.innerText = `The computer chose ${computerChoice}. You lost!`;
+        console.log(`The computer chose ${computerChoice}. You lost!`)
     }
+
+    player.innerText = `Player: ${playerScore}`;
+    computer.innerText = `Computer: ${computerScore}`;
 }
 
-game();
+
+function reset() {
+    playerScore = 0;
+    computerScore = 0;
+    statusDiv.innerText = "First to 5 points wins!";
+    player.innerText = `Player: ${playerScore}`;
+    computer.innerText = `Computer: ${computerScore}`;
+
+    endContainer.style.opacity = 0;
+    container.style.opacity = 100;
+
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+    scissorsBtn.disabled = false;
+
+    playAgain.disabled = true;
+}
+
+function showEndScreen() {
+    container.style.opacity = 0;
+    endContainer.style.opacity = 100;
+
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+
+    playAgain.disabled = false;
+}
+
+
+rockBtn.addEventListener("click", () => handleClick("rock"));
+paperBtn.addEventListener("click", () => handleClick("paper"));
+scissorsBtn.addEventListener("click", () => handleClick("scissors")); 
+playAgain.addEventListener("click", reset); 
